d72359acd64b725331522db6cef6ecab84877f81
Joshua Collis <joshua.d.collis@gmail.com>
2024-06-19 17:10:45 -0400

    Enemy Prototype fixed

---
+++ Game/Enemy/EnemyController.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 150
+@export var initial_move_direction : int = 1
+
+var move_direction : int
+
+func _ready():
+	move_direction = initial_move_direction
+
+func _physics_process(delta):
+	velocity.x = move_speed * move_direction
+	
+	if !is_on_floor():
+		velocity.y += 9.8
+	else:
+		velocity.y = 0
+	
+	
+	
+	move_and_slide()
+
+
+func _on_area_2d_body_entered(body):
+	#if body.is_in_group("Player"):
+	
+	move_direction *= -1
